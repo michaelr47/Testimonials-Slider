eb7f454f5337bf8ca3ee22b59d4f24064f9e27a3
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-07-13 21:40:04 -0700

    added bg images

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let images = ['./images/image-john.jpg', './images/image-tanya.jpg'];
+// let images = ['./images/image-john.jpg', './images/image-tanya.jpg'];
 
-function displayImages(arrOfImages) {
-    arrOfImages.forEach((img, i) => {
-        let image = document.createElement('img');
-        image.src = arrOfImages[i];
-        document.body.append(image); 
-    })
-}
+// function displayImages(arrOfImages) {
+//     arrOfImages.forEach((img, i) => {
+//         let image = document.createElement('img');
+//         image.src = arrOfImages[i];
+//         document.body.append(image); 
+//     })
+// }
 
-displayImages(images);
+// displayImages(images);
