c8b6dfcc88e242c788999a6241a39835e025900d
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-07-20 22:07:35 -0700

    feat two functions for prev and next btn to change image

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-// let images = ['./images/image-john.jpg', './images/image-tanya.jpg'];
 
-// function displayImages(arrOfImages) {
-//     arrOfImages.forEach((img, i) => {
-//         let image = document.createElement('img');
-//         image.src = arrOfImages[i];
-//         document.body.append(image); 
-//     })
-// }
+let images = ['./images/image-john.jpg', './images/image-tanya.jpg'];
+let prevBtn = document.querySelector('.prevBtn');
+let nextBtn = document.querySelector('.nextBtn');
+let idx = 0;
+let img = document.querySelector('.imgContainer img');
 
-// displayImages(images);
-
-function changeImage(img) {
-    let index = 0;
-    if (index === 0) {
-        // return tanya
-
-    }
-
-    // otherwise 
-        // append next index 
+function changeToPrevImage() {
+   idx--;
+   if (idx < 0) {
+    idx = (images.length - 1)
+   }
+   img.src = images[idx];
+    
 }
+
+function changeToNextImage() {
+    idx++;
+    if (idx === images.length) {
+        idx = 0;
+    }
+    img.src = images[idx];
+}
+
+prevBtn.addEventListener('click', changeToPrevImage);
+nextBtn.addEventListener('click', changeToNextImage);
