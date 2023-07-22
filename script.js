09977192e1d72ee35fc50524da16cd049f9501f3
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-07-21 22:17:28 -0700

    trying an array of objects, work in progress

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
-let images = ['./images/image-john.jpg', './images/image-tanya.jpg'];
+// let images = ['./images/image-john.jpg', './images/image-tanya.jpg'];
 let prevBtn = document.querySelector('.prevBtn');
 let nextBtn = document.querySelector('.nextBtn');
+let quote = document.querySelector('.testimony-Header h1');
+console.log(quote);
 let idx = 1;
 let img = document.querySelector('.imgContainer img');
 
@@ -24,5 +26,40 @@ function changeToNextImage() {
 
 }
 
+// window.onload = function() {
+//     if (images[0]) {
+        
+//     }
+// }
+
 prevBtn.addEventListener('click', changeToPrevImage);
 nextBtn.addEventListener('click', changeToNextImage);
+
+
+
+
+
+
+const testimonies = [
+    {
+        id: 'Tanya Sinclair',
+        quote: `I’ve been interested in coding for a while but never taken the jump, until now. 
+        I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
+        excited about the future.`,
+        occupation: 'UX Engineer',
+        imageURL: './images/image-tanya.jpg'
+    },
+    
+    {
+        id: 'John Tarkpor',
+        quote: `“ If you want to lay the best foundation possible I’d recommend taking this course. 
+        The depth the instructors go into is incredible. I now feel so confident about 
+        starting up as a professional developer. ”`,
+        occupation: 'Junior Front-end Developer',
+        imageURL: './images/image-john.jpg'
+    }
+]
+
+for (const test of testimonies) {
+    console.log(test);
+}
