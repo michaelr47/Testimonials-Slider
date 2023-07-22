c6286ff57a3e35610cb08867ffcc32942d8eca73
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-07-21 20:45:34 -0700

    fixed bug where buttons would now respond to first click

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let images = ['./images/image-john.jpg', './images/image-tanya.jpg'];
 let prevBtn = document.querySelector('.prevBtn');
 let nextBtn = document.querySelector('.nextBtn');
-let idx = 0;
+let idx = 1;
 let img = document.querySelector('.imgContainer img');
 
 function changeToPrevImage() {
@@ -11,7 +11,7 @@ function changeToPrevImage() {
     idx = (images.length - 1)
    }
    img.src = images[idx];
-    
+   return img;
 }
 
 function changeToNextImage() {
@@ -20,6 +20,8 @@ function changeToNextImage() {
         idx = 0;
     }
     img.src = images[idx];
+    return img;
+
 }
 
 prevBtn.addEventListener('click', changeToPrevImage);
