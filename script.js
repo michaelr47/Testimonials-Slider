9c644dab070f8da77f3c11a0938102850c03c065
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-07-23 18:47:20 -0700

    changed var. name and added comments

--- script.js
+++ script.js
@@ -1,55 +1,59 @@
+// previous and next buttons
+const prevBtn = document.querySelector(".prevBtn");
+const nextBtn = document.querySelector(".nextBtn");
+// accessing testimony DOM elements
+const quote = document.querySelector(".testimony-Header h1");
+const personName = document.querySelector(".name");
+const occupation = document.querySelector(".occupation");
+const img = document.querySelector(".imgContainer img");
+// change slides index
+let slidingIdx = 0; 
 
-const prevBtn = document.querySelector('.prevBtn');
-const nextBtn = document.querySelector('.nextBtn');
-const quote = document.querySelector('.testimony-Header h1');
-const personName = document.querySelector('.name');
-const occupation = document.querySelector('.occupation');
-const img = document.querySelector('.imgContainer img');
-let idx = 0;
-
-
+// testimonies data 
 const testimonies = [
-    {
-        id: 'Tanya Sinclair',
-        quote: `" I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. " `,
-        occupation: 'UX Engineer',
-        imageURL: './images/image-tanya.jpg'
-    },
-    
-    {
-        id: 'John Tarkpor',
-        quote: `“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”`,
-        occupation: 'Junior Front-end Developer',
-        imageURL: './images/image-john.jpg'
-    }
-]
+  {
+    id: "Tanya Sinclair",
+    quote: `" I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. " `,
+    occupation: "UX Engineer",
+    imageURL: "./images/image-tanya.jpg",
+  },
+
+  {
+    id: "John Tarkpor",
+    quote: `“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”`,
+    occupation: "Junior Front-end Developer",
+    imageURL: "./images/image-john.jpg",
+  },
+];
 
 function changeToPrevImage() {
-   idx--;
+  slidingIdx--;
 
-   if (idx < 0) {
-    idx = (testimonies.length - 1)
-   }
-   
-   quote.innerText = testimonies[idx].quote;
-   personName.innerText = testimonies[idx].id;
-   occupation.innerText = testimonies[idx].occupation;
-   img.src = testimonies[idx].imageURL;
+
+
+  if (slidingIdx < 0) {
+    slidingIdx = testimonies.length - 1;
+  }
+
+  quote.innerText      = testimonies[slidingIdx].quote;
+  personName.innerText = testimonies[slidingIdx].id;
+  occupation.innerText = testimonies[slidingIdx].occupation;
+  img.src              = testimonies[slidingIdx].imageURL;
 
 }
 
 function changeToNextImage() {
-    idx++;
-    
-    if (idx === testimonies.length) {
-        idx = 0;
-    }
+  slidingIdx++;
 
-    quote.innerText = testimonies[idx].quote;
-    personName.innerText = testimonies[idx].id;
-    occupation.innerText = testimonies[idx].occupation;
-    img.src = testimonies[idx].imageURL;
+  if (slidingIdx === testimonies.length) {
+    slidingIdx = 0;
+  }
+
+  quote.innerText      = testimonies[slidingIdx].quote;
+  personName.innerText = testimonies[slidingIdx].id;
+  occupation.innerText = testimonies[slidingIdx].occupation;
+  img.src              = testimonies[slidingIdx].imageURL;
 }
 
-prevBtn.addEventListener('click', changeToPrevImage);
-nextBtn.addEventListener('click', changeToNextImage);
+prevBtn.addEventListener("click", changeToPrevImage);
+nextBtn.addEventListener("click", changeToNextImage);
